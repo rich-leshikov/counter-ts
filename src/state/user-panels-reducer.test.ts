a03cdc82339be5156fbcba4d6b6e9685ceca1b2d
Richard Leshik <richardleshik@gmail.com>
2023-06-25 18:29:05 +0300

    add tests for reducer

--- src/state/user-panels-reducer.test.ts
+++ src/state/user-panels-reducer.test.ts
@@ -1,6 +1,6 @@
 import {
   incrementCounterAC,
-  resetCounterAC,
+  resetCounterAC, setMaxValue, setStartValue,
   userPanelId1,
   userPanelId2,
   userPanelsReducer,
@@ -20,7 +20,7 @@ beforeEach(() => {
 
 test('counter of user panel should be changed', () => {
   const endState = userPanelsReducer(startState, incrementCounterAC())
-  // console.log(endState)
+
   expect(endState[1].count).toBe(1)
   expect(endState.length).toBe(2)
 })
@@ -28,6 +28,22 @@ test('counter of user panel should be changed', () => {
 test('counter should be reset to zero', () => {
   const endState = userPanelsReducer(startState, resetCounterAC())
 
+  expect(endState[1].count).toBe(0)
+  expect(endState.length).toBe(2)
+})
+
+test('start value should be changed', () => {
+  const endState = userPanelsReducer(startState, setStartValue(3))
+
+  expect(endState[1].startValue).toBe(3)
+  expect(endState[1].count).toBe(3)
+  expect(endState.length).toBe(2)
+})
+
+test('max value should be changed', () => {
+  const endState = userPanelsReducer(startState, setMaxValue(5))
+
+  expect(endState[1].maxValue).toBe(5)
   expect(endState[1].count).toBe(0)
   expect(endState.length).toBe(2)
 })
