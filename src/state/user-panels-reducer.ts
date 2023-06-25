5d824e0792427510d928c119c4f5f578765eedad
Richard Leshik <richardleshik@gmail.com>
2023-06-25 16:52:11 +0300

    change user panels reducer

--- src/state/user-panels-reducer.ts
+++ src/state/user-panels-reducer.ts
@@ -3,38 +3,48 @@ export const userPanelId1 = 'userPanelId1'
 export const userPanelId2 = 'userPanelId2'
 
 const initialState: Array<UserPanelType> = [
-  {id: userPanelId1, displayType: 'setter', count: 0},
-  {id: userPanelId2, displayType: 'counter', count: 0},
+  {id: userPanelId1, userPanelType: 'setter'},
+  {id: userPanelId2, userPanelType: 'counter', count: 0, startValue: 0, maxValue: 0}
 ]
 
 // reducer
 export const userPanelsReducer = (state: Array<UserPanelType> = initialState, action: ActionType): Array<UserPanelType> => {
   switch (action.type) {
     case 'INCREMENT-COUNTER':
-      return state.map(up => up.id === action.userPanelId ? {...up, count: up.count + 1} : up)
+      return state.map(up => up.count && up.userPanelType === 'counter' ? {...up, count: up.count + 1} : up)
     case 'RESET-COUNTER':
-      return state.map(up => up.id === action.userPanelId ? {...up, count: 0} : up)
+      return state.map(up => up.count && up.userPanelType === 'counter' ? {...up, count: up.startValue} : up)
+    case 'SET-START-VALUE':
+      return state.map(up => up.startValue && up.userPanelType === 'counter' ? {...up, startValue: action.startValue} : up)
+    case 'SET-MAX-VALUE':
+      return state.map(up => up.maxValue && up.userPanelType === 'counter' ? {...up, maxValue: action.maxValue} : up)
     default:
       return state
   }
 }
 
 // actions
-export const incrementCounterAC = (userPanelId: string) =>
-  ({type: 'INCREMENT-COUNTER', userPanelId} as const)
-export const resetCounterAC = (userPanelId: string) =>
-  ({type: 'RESET-COUNTER', userPanelId} as const)
+export const incrementCounterAC = () => ({type: 'INCREMENT-COUNTER'} as const)
+export const resetCounterAC = () => ({type: 'RESET-COUNTER'} as const)
+export const setStartValue = (startValue: number) => ({type: 'SET-START-VALUE', startValue} as const)
+export const setMaxValue = (maxValue: number) => ({type: 'SET-MAX-VALUE', maxValue} as const)
 
 // types
 export type IncrementCounterType = ReturnType<typeof incrementCounterAC>
 export type ResetCounterType = ReturnType<typeof resetCounterAC>
+export type ResetStartValueType = ReturnType<typeof setStartValue>
+export type ResetMaxValueType = ReturnType<typeof setMaxValue>
 export type ActionType =
   | IncrementCounterType
   | ResetCounterType
+  | ResetStartValueType
+  | ResetMaxValueType
 
-export type DisplayType = 'setter' | 'counter'
+export type userPanelType = 'setter' | 'counter'
 export type UserPanelType = {
   id: string
-  displayType: DisplayType
-  count: number //shouldn't be assigned to setter display type
+  userPanelType: userPanelType
+  count?: number
+  startValue?: number
+  maxValue?: number
 }
