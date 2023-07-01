063678d32b9ed9551f49f5d4225160db6d54788a
Richard Leshik <richardleshik@gmail.com>
2023-07-01 18:00:17 +0300

    add local storage functions

---
+++ src/utils/localStorage.ts
@@ -0,0 +1,20 @@
+import {AppRootStateType} from '../state/store';
+
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('state')
+    if (serializedState === null) {
+      return undefined
+    }
+    return JSON.parse(serializedState)
+  } catch (err) {
+    return undefined
+  }
+}
+
+export const saveState = (state: AppRootStateType) => {
+  try {
+    localStorage.setItem('state', JSON.stringify(state))
+  } catch {}
+}
+
