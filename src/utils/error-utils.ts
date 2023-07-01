ad10d8ebbd547b4e840d564b702f332519343566
Richard Leshik <richardleshik@gmail.com>
2023-07-01 17:59:46 +0300

    add util for error message

---
+++ src/utils/error-utils.ts
@@ -0,0 +1,15 @@
+import {Dispatch} from 'redux'
+import {setInfoMessageAC, SetInfoMessageType} from '../state/user-panels-reducer';
+
+
+// generic function
+export const handleInfoMessageError = (hasError: boolean, dispatch: ErrorUtilsDispatchType) => {
+  if (hasError) {
+    dispatch(setInfoMessageAC('incorrect value!'))
+  } else {
+    dispatch(setInfoMessageAC(`enter values and press 'set'`))
+  }
+}
+
+
+type ErrorUtilsDispatchType = Dispatch<SetInfoMessageType>
